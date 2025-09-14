6a78d92e0afcb6a51bfdc159938ef74a3de10bda
amiraalb <208763343+amiraalb@users.noreply.github.com>
2025-09-14 10:14:25 +0200

    Added new files

---
+++ ÖvningarFL3/jokes.py
@@ -0,0 +1,16 @@
+import random, numpy as np
+
+programming_jokes = np.array([
+  "Why don't programmers like nature? It has too many bugs.",
+  "Why did the CSS developer go to therapy? To get rid of his margins.",
+  "How do you comfort a JavaScript developer? You console them.",
+  "Why did the CSS developer leave the restaurant? Because it had no class.",
+  "Why did the JavaScript developer go missing? Because he didn't know when to return.",
+  "Why did the HTML tag go to the party? Because it wanted to break the line.",
+  "Why do JavaScript developers wear glasses? Because they don't C#.",
+  "Why don't programmers like to use inline styles? Because they want to be classy.",
+  "Why did the CSS selector break up with the HTML element? It found someone more specific.",
+  "Why did the CSS developer apply for a job? They wanted to get a position.",
+])
+
+print(np.random.choice(programming_jokes, size=2, replace=False))
---
+++ ÖvningarFL3/sten_sax_pase.py
@@ -0,0 +1,34 @@
+"""
+Lägg även till så att man kan spela igen och antalet vinster / förluster hålls reda på.
+
+Experimentera med att dela upp koden i moduler.
+"""
+import random
+def sten_sax_pase():
+    min_lista = ["Sten", "Sax", "Påse"]
+
+    val = input("Skriv in sten, sax, eller påse:\n").capitalize()
+    print(f"Du har valt: {val}.")
+
+    computer_choice = random.choice(min_lista)
+    print(f"Datorn väljer: {computer_choice}.")
+
+    if val == computer_choice:
+        print("Du och datorn har valt samma! Det är oavgjort.")
+    elif val == "Sten" and computer_choice == "Sax":
+        print("Du vann!")
+    elif val == "Sten" and computer_choice == "Påse":
+        print("Datorn vann!")
+    elif val == "Sax" and computer_choice == "Sten":
+        print("Datorn vann!")
+    elif val == "Sax" and computer_choice == "Påse":
+        print("Du vann!")
+    elif val == "Påse" and computer_choice == "Sten":
+        print("Du vann!")
+    elif val == "Påse" and computer_choice == "Sax":
+        print("Datorn vann!")
+
+
+
+
+sten_sax_pase()
